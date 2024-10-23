df57207b708f090aa889ce1bf3ff9c07eaa58706
Vishnuram Jatin Bangaru <46157313+vishnuramj10@users.noreply.github.com>
2024-10-23 12:46:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def ask():
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000, debug=True)
-    serve(app, host="127.0.0.1", port = 8081)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    #serve(app, host="127.0.0.1", port = 8081)
